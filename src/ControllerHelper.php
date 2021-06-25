c1021a5addf7fb1c1203a213ea984392d6aacfea
Pozitronik <antikillerxxl@gmail.com>
2021-06-25 22:50:41 +0300

    Trait methods refactored

--- src/ControllerHelper.php
+++ src/ControllerHelper.php
@@ -5,12 +5,9 @@ namespace pozitronik\helpers;
 
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
-use ReflectionException;
 use Throwable;
 use Yii;
 use yii\base\InvalidConfigException;
-use yii\base\UnknownClassException;
-use yii\helpers\Url;
 
 /**
  * Class ControllerHelper
@@ -69,7 +66,6 @@ class ControllerHelper {
 	 */
 	public static function GetControllersList(string $path, ?string $moduleId = null, ?array $parentClassFilter = null):array {
 		$result = [];
-
 		$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(Yii::getAlias($path)), RecursiveIteratorIterator::SELF_FIRST);
 		/** @var RecursiveDirectoryIterator $file */
 		foreach ($files as $file) {
@@ -78,52 +74,6 @@ class ControllerHelper {
 			}
 		}
 		return $result;
-	}
-
-	/**
-	 * По имени экшена возвращает абсолютный Url в приложении
-	 * @param string $action
-	 * @param array $params
-	 * @return array
-	 * @example SomeController::GetActionUrl('index', ['id' => 1]) => ['/some/index', 'id' => 1]
-	 */
-	public static function GetActionUrl(string $action, array $params = []):array {
-		$controllerId = static::ExtractControllerId(static::class);
-		$route = Utils::setAbsoluteUrl($controllerId.Utils::setAbsoluteUrl($action));
-		if ([] !== $params) {
-			array_unshift($params, $route);
-		} else {
-			$params = [$route];
-		}
-		return $params;
-	}
-
-	/**
-	 * По имени экшена возвращает строковой абсолютный Url в приложении
-	 * @param string $action
-	 * @param array $params
-	 * @param bool|string $scheme @see Url::to() $scheme parameter
-	 * @return string
-	 * @example SomeController::to('index', ['id' => 1]) => '/some/index?id=1'
-	 */
-	public static function to(string $action, array $params = [], $scheme = false):string {
-		return Url::to(self::GetActionUrl($action, $params), $scheme);
-	}
-
-	/**
-	 * Возвращает все экшены контроллера
-	 * @param bool $asRequestName Привести имя экшена к виду в запросе
-	 * @return string[]
-	 * @throws ReflectionException
-	 * @throws UnknownClassException
-	 */
-	public static function GetControllerActions(bool $asRequestName = true):array {
-		$names = ArrayHelper::getColumn(ReflectionHelper::GetMethods(static::class), 'name');
-		$names = preg_filter('/^action([A-Z])(\w+?)/', '$1$2', $names);
-		if ($asRequestName) {
-			foreach ($names as &$name) $name = self::GetActionRequestName($name);
-		}
-		return $names;
 	}
 
 	/**
