403d0e8ce9372e255ed41bde495f707a46870e1f
Pozitronik <antikillerxxl@gmail.com>
2021-12-15 15:05:29 +0300

    PHP 8.0 conversion + new methods

--- src/ControllerHelper.php
+++ src/ControllerHelper.php
@@ -5,9 +5,12 @@ namespace pozitronik\helpers;
 
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
+use ReflectionException;
 use Throwable;
 use Yii;
+use yii\base\Controller;
 use yii\base\InvalidConfigException;
+use yii\base\UnknownClassException;
 
 /**
  * Class ControllerHelper
@@ -48,7 +51,7 @@ class ControllerHelper {
 	 * @throws InvalidConfigException
 	 * @throws Throwable
 	 */
-	public static function GetControllerByControllerId(string $controllerId, ?string $moduleId):?object {
+	public static function GetControllerByControllerId(string $controllerId, ?string $moduleId = null):?object {
 		if (null === $module = ModuleHelper::GetModuleById($moduleId)) throw new InvalidConfigException("Module $moduleId not found or module not configured properly.");
 		$controllerId = implode('', array_map('ucfirst', preg_split('/-/', $controllerId, -1, PREG_SPLIT_NO_EMPTY)));
 		return self::LoadControllerClassFromFile("{$module->controllerPath}/{$controllerId}Controller.php", $moduleId);
@@ -77,6 +80,22 @@ class ControllerHelper {
 	}
 
 	/**
+	 * Возвращает список контроллеров в указанных каталогах
+	 * @return string[]
+	 * @throws Throwable
+	 */
+	public static function GetControllersFromPath(array $controllerDirs = ['@app/controllers']):array {
+		$result = [];
+		foreach ($controllerDirs as $controllerDir => $idPrefix) {
+			$controllers = self::GetControllersList((string)$controllerDir, null, [Controller::class]);
+			$result[$controllerDir] = ArrayHelper::map($controllers, static fn(Controller $model) => ('' === $idPrefix)
+				?$model->id
+				:$idPrefix.'/'.$model->id, static fn(Controller $model) => ('' === $idPrefix)?$model->id:$idPrefix.'/'.$model->id);
+		}
+		return $result;
+	}
+
+	/**
 	 * Переводит вид имени экшена к виду запроса, который этот экшен дёргает.
 	 * @param string $action
 	 * @return string
@@ -100,4 +119,22 @@ class ControllerHelper {
 		$controllerName = preg_replace('/(^.+)(\\\)([A-Z].+)(Controller$)/', '$3', $className);//app/shit/BlaBlaBlaController => BlaBlaBla
 		return mb_strtolower(implode('-', preg_split('/([[:upper:]][[:lower:]]+)/', $controllerName, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY)));
 	}
+
+	/**
+	 * Возвращает все экшены контроллера
+	 * @param string $controller_class
+	 * @param bool $asRequestName Привести имя экшена к виду в запросе
+	 * @return string[]
+	 * @throws ReflectionException
+	 * @throws UnknownClassException
+	 */
+	public static function GetControllerActions(string $controller_class, bool $asRequestName = true):array {
+		$names = ArrayHelper::getColumn(ReflectionHelper::GetMethods($controller_class), 'name');
+		$names = preg_filter('/^action([A-Z])(\w+?)/', '$1$2', $names);
+		if ($asRequestName) {
+			foreach ($names as &$name) $name = self::GetActionRequestName($name);
+		}
+		return $names;
+	}
+
 }
