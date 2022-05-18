11261a364bcf17d7b4d58b9eafbc3e5da1d19cb7
Pozitronik <antikillerxxl@gmail.com>
2022-05-18 09:26:44 +0300

    #tests Инициализация тестового окружения

---
+++ tests/_support/Extensions/DatabaseMigrationExtension.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+
+namespace Extensions;
+
+use Codeception\Events;
+use Codeception\Extension;
+use Codeception\Module\Cli;
+
+/**
+ * Class DatabaseMigrationExtension
+ */
+class DatabaseMigrationExtension extends Extension {
+	public static $events = [
+		Events::SUITE_BEFORE => 'beforeSuite',
+		Events::TEST_BEFORE => 'beforeSuite'
+	];
+
+	public function beforeSuite():void {
+		/** @var Cli $cli */
+		$cli = $this->getModule('Cli');
+		$alias = __DIR__.'/../../_app/yii';
+		$cli->runShellCommand("php $alias migrate/fresh --interactive=0");
+		$cli->runShellCommand("php $alias migrate/up --migrationPath=./migrations --interactive=0");
+		$cli->runShellCommand("php $alias migrate/up --migrationPath=@vendor/pozitronik/yii2-dynamic-attributes/migrations --interactive=0");
+	}
+}
