17a46b59ece79479537344a95482c050b65438f5
Pozitronik <antikillerxxl@gmail.com>
2022-05-18 10:16:27 +0300

    Реализация методов isEqual() (сравнение массивов) и isInWithFloatDelta() (поиск с заданной точностью содержимого с плавающей точкой) + тесты

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace pozitronik\helpers;
 
+use Traversable;
 use function array_keys;
 use Closure;
 use Throwable;
@@ -12,6 +13,10 @@ use yii\helpers\ArrayHelper as YiiArrayHelper;
  * Class ArrayHelper
  */
 class ArrayHelper extends YiiArrayHelper {
+	public const FLAG_COMPARE_KEYS = 1;//наборы ключей должны совпадать
+	public const FLAG_COMPARE_VALUES = 2;//наборы значений должны совпадать
+	public const FLAG_COMPARE_KEY_VALUES_PAIRS = 4;//наборы ключ-значение должны совпадать
+	public const FLOAT_DELTA = 1.0E-13;//Дельта сравнения двух чисел с плавающей точкой внутри PHP
 
 	/**
 	 * Шорткат для объединения массива с массивом, полученным в цикле
@@ -395,4 +400,81 @@ class ArrayHelper extends YiiArrayHelper {
 		return array_combine(str_replace($search, $replace, array_keys($array)), $array);
 	}
 
+	/**
+	 * Сравнивает два массива между собой по наборам данных (с учётом вложенности).
+	 * Сравнение всегда строгое
+	 * @param array|Traversable $array_one
+	 * @param array|Traversable $array_two
+	 * @param int $flags
+	 * @return bool
+	 */
+	public static function isEqual(array|Traversable $array_one, array|Traversable $array_two, int $flags = self::FLAG_COMPARE_KEYS + self::FLAG_COMPARE_VALUES + self::FLAG_COMPARE_KEY_VALUES_PAIRS):bool {
+		if (count($array_one) !== count($array_two)) return false;
+		foreach ($array_one as $a1key => $a1value) {
+			if (($flags & self::FLAG_COMPARE_KEYS) && !static::keyExists($a1key, $array_two)) {//разница по ключам
+				return false;
+			}
+			if (($flags & self::FLAG_COMPARE_VALUES) && !static::isInWithFloatDelta($a1value, $array_two, true)) {//разница по значениям
+				return false;
+			}
+			if ($flags & self::FLAG_COMPARE_KEY_VALUES_PAIRS) {
+				if (!static::keyExists($a1key, $array_two) || !static::isEquals($a1value, $array_two[$a1key])) {
+					return false;
+				}
+			}
+			if (static::isTraversable($a1value) && static::isTraversable($array_two[$a1key]??null) && false === static::isEqual($a1value, $array_two[$a1key]??null)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Функция аналогична static::isIn() но для поиска float-значений учитывает дельту PHP
+	 * @param mixed $needle
+	 * @param array|Traversable $haystack
+	 * @param bool $strict
+	 * @return bool
+	 */
+	public static function isInWithFloatDelta(mixed $needle, array|Traversable $haystack, bool $strict = false):bool {
+		if (is_float($needle)) {
+			foreach ($haystack as $value) {
+				if (is_float($value) && static::isFloatEquals($needle, $value)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		return static::isIn($needle, $haystack, $strict);
+	}
+
+	/**
+	 * @param mixed $floatOne
+	 * @param mixed $floatTwo
+	 * @param float $delta
+	 * @return bool
+	 */
+	public static function isFloatEquals(mixed $floatOne, mixed $floatTwo, float $delta = self::FLOAT_DELTA):bool {
+		if (is_infinite($floatTwo) && is_infinite($floatOne)) {
+			return true;
+		}
+
+		if ((is_infinite($floatTwo) xor is_infinite($floatOne)) ||
+			(is_nan($floatTwo) || is_nan($floatOne)) ||
+			abs($floatTwo - $floatOne) > $delta) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @param mixed $scalarOne
+	 * @param mixed $scalarTwo
+	 * @return bool
+	 */
+	private static function isEquals(mixed $scalarOne, mixed $scalarTwo):bool {
+		if (is_float($scalarOne) && is_float($scalarTwo)) return static::isFloatEquals($scalarOne, $scalarTwo);
+		return $scalarOne === $scalarTwo;
+	}
 }
--- tests/unit/ArrayHelperTest.php
+++ tests/unit/ArrayHelperTest.php
@@ -48,4 +48,65 @@ class ArrayHelperTest extends Unit {
 		], $testArray);
 	}
 
+
+	/**
+	 * @return void
+	 */
+	public function testIsEqual():void {
+		$array_one = ["red", "green", "blue"];
+		$array_two = ["red", "green", "yellow"];
+		$array_three = ["red", "blue", "green"];
+		$array_four = ["a" => "red", "b" => "green", "c" => "yellow"];
+		$array_five = ["x" => "red", "z" => "yellow", "y" => "green"];
+		$array_six = ["x" => "red", "z" => "yellow", "y" => "green"];
+		$array_seven = ["null", null, "not null"];
+		$array_eight = ["c" => "not null", "b" => "null", "a" => null];
+		$array_nine = [[M_PI, [["a" => "red"]]], [null, "string", 0], [false, true]];
+		$array_ten = ["a" => [M_PI, [["a" => 'red']]], "b" => [null, "string", 0], "c" => [false, true]];
+
+		self::assertTrue(ArrayHelper::isEqual($array_one, $array_two, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertFalse(ArrayHelper::isEqual($array_one, $array_two, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertFalse(ArrayHelper::isEqual($array_one, $array_two, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+		self::assertFalse(ArrayHelper::isEqual($array_one, $array_two, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS + ArrayHelper::FLAG_COMPARE_KEYS + ArrayHelper::FLAG_COMPARE_VALUES));
+
+		self::assertTrue(ArrayHelper::isEqual($array_one, $array_three, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_one, $array_three, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertFalse(ArrayHelper::isEqual($array_one, $array_three, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+		self::assertTrue(ArrayHelper::isEqual($array_one, $array_three, ArrayHelper::FLAG_COMPARE_KEYS + ArrayHelper::FLAG_COMPARE_VALUES));
+
+		self::assertFalse(ArrayHelper::isEqual($array_four, $array_five, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_four, $array_five, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertFalse(ArrayHelper::isEqual($array_four, $array_five, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+
+		self::assertTrue(ArrayHelper::isEqual($array_five, $array_six, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_five, $array_six, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertTrue(ArrayHelper::isEqual($array_five, $array_six, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+		self::assertTrue(ArrayHelper::isEqual($array_five, $array_six, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS + ArrayHelper::FLAG_COMPARE_KEYS + ArrayHelper::FLAG_COMPARE_VALUES));
+
+		self::assertTrue(ArrayHelper::isEqual($array_one, $array_seven, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertFalse(ArrayHelper::isEqual($array_one, $array_seven, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertFalse(ArrayHelper::isEqual($array_one, $array_seven, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+
+		self::assertTrue(ArrayHelper::isEqual($array_seven, $array_seven, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_seven, $array_seven, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertTrue(ArrayHelper::isEqual($array_seven, $array_seven, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+
+		self::assertFalse(ArrayHelper::isEqual($array_seven, $array_eight, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_seven, $array_eight, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertFalse(ArrayHelper::isEqual($array_seven, $array_eight, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+
+		self::assertTrue(ArrayHelper::isEqual($array_nine, $array_nine, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_nine, $array_nine, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertTrue(ArrayHelper::isEqual($array_nine, $array_nine, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+
+		self::assertFalse(ArrayHelper::isEqual($array_nine, $array_ten, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_nine, $array_ten, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertFalse(ArrayHelper::isEqual($array_nine, $array_ten, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+
+		self::assertTrue(ArrayHelper::isEqual($array_ten, $array_ten, ArrayHelper::FLAG_COMPARE_KEYS));
+		self::assertTrue(ArrayHelper::isEqual($array_ten, $array_ten, ArrayHelper::FLAG_COMPARE_VALUES));
+		self::assertTrue(ArrayHelper::isEqual($array_ten, $array_ten, ArrayHelper::FLAG_COMPARE_KEY_VALUES_PAIRS));
+
+	}
+
 }
