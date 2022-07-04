837409f9cfeb12b13909b0542ef9118f52cdf796
Pozitronik <antikillerxxl@gmail.com>
2022-07-04 15:38:35 +0300

    code style

--- tests/unit/UtilsTest.php
+++ tests/unit/UtilsTest.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 use Codeception\Test\Unit;
 use pozitronik\helpers\Utils;
@@ -7,33 +7,31 @@ use pozitronik\helpers\Utils;
 /**
  * class UtilsTest
  */
-class UtilsTest extends Unit
-{
-    /**
-     * @var UnitTester
-     */
-    protected $tester;
+class UtilsTest extends Unit {
+	/**
+	 * @var UnitTester
+	 */
+	protected $tester;
 
-    /**
-     * @return void
-     */
-    public function testSomeFeature():void
-    {
-        $this->tester->assertEquals(
-            "name: Ошибка #1: 0: Внутренняя ошибка #1\nОшибка #2: 0: Внутренняя ошибка #2\nОшибка #3: 0: Внутренняя ошибка #4",
-            Utils::Errors2String([
-                'name' => [
-                    'Ошибка #1' => [
-                        'Внутренняя ошибка #1'
-                    ],
-                    'Ошибка #2' => [
-                        'Внутренняя ошибка #2',
-                        'Ошибка #3' => [
-                            'Внутренняя ошибка #4'
-                        ],
-                    ]
-                ]
-            ])
-        );
-    }
+	/**
+	 * @return void
+	 */
+	public function testSomeFeature():void {
+		$this->tester->assertEquals(
+			"name: Ошибка #1: 0: Внутренняя ошибка #1\nОшибка #2: 0: Внутренняя ошибка #2\nОшибка #3: 0: Внутренняя ошибка #4",
+			Utils::Errors2String([
+				'name' => [
+					'Ошибка #1' => [
+						'Внутренняя ошибка #1'
+					],
+					'Ошибка #2' => [
+						'Внутренняя ошибка #2',
+						'Ошибка #3' => [
+							'Внутренняя ошибка #4'
+						],
+					]
+				]
+			])
+		);
+	}
 }
